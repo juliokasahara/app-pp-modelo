f544aa670c88f10a89cb35b022fb00e84750fccd
Julio <juliokasahara@gmail.com>
2021-07-05 21:26:32 -0300

    primeiro commit

---
+++ src/main/java/br/com/app/modelo/domain/AppPpModeloApplication.java
@@ -0,0 +1,13 @@
+package br.com.app.modelo.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPpModeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPpModeloApplication.class, args);
+	}
+
+}
